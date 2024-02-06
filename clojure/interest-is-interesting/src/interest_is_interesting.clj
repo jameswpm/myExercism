a61825c60d9799a5950c09eebe6d046cf4ac1aba
James Miranda <jameswpm@gmail.com>
2024-02-06 17:11:39 +0100

    Refactor interest-rate, annual-balance-update, and amount-to-donate functions

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,19 +1,32 @@
 (ns interest-is-interesting)
 
+(require '[clojure.math :refer [ceil floor round]])
+
 (defn interest-rate
   "Returns the interest rate based on the specified balance."
   [balance]
-  (cond (< balance 0) -3.213
-        (and (>= balance 0) (< balance 1000)) 0.5
-        (and (>= balance 0) (>= balance 1000)) 1.621
-        (and (>= balance 0) (>= balance 5000)) 2.475))
+  (double 
+    (cond (< balance 0) -3.213
+          (>= balance 5000) 2.475
+          (>= balance 1000) 1.621
+          (< balance 1000) 0.5)))
 
 (defn annual-balance-update
   "Returns the annual balance update, taking into account the interest rate."
   [balance]
-  )
+  (double
+    (let [rate (interest-rate balance)]
+    (if (> rate 0)
+      (* balance (/ rate 100))
+      (* balance (- 1 (/ rate 100)))))))
 
 (defn amount-to-donate
   "Returns how much money to donate based on the balance and the tax-free percentage."
   [balance tax-free-percentage]
-  )
+  (int 
+    (floor 
+      (if 
+        (> balance 0) 
+        (* (* balance (/ tax-free-percentage 100)) 2) 
+        0
+      ))))
