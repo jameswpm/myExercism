b0eab2418e091880f11dc2627cfb9dd34dd1f470
James Miranda <jameswpm@gmail.com>
2024-02-07 16:39:15 +0100

    Add functions to modify language list in the PHP exercise

--- php/language-list/LanguageList.php
+++ php/language-list/LanguageList.php
@@ -1,6 +1,23 @@
 <?php
 
-function language_list()
+function language_list(...$langs)
 {
-    // implement the language list function
+    return $langs;
+}
+
+function add_to_language_list($language_list, $new_lang) {
+    $language_list[] = $new_lang;
+    return $language_list;
+}
+
+function prune_language_list($language_list){
+    return array_slice($language_list, 1);
+}
+
+function current_language($language_list){
+    return $language_list[0];
+}
+
+function language_list_length($language_list){
+    count($language_list);
 }
